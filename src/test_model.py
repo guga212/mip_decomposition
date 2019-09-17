604104c48cf296b06ecf028920a6e10b2f0d2e05
Ignacy Ruksza <ignacy.ruksza@gmail.com>
2019-09-17 20:58:42 +0000

    Updated model generator test.

--- src/test_model.py
+++ src/test_model.py
@@ -5,7 +5,7 @@ import SolverManager as sm
 
 #random network
 net.NetworkGraph.SetRandomSeed(231)
-network = net.NetworkGraph.GenerateRandom(8, 28, 0, 2)
+network = net.NetworkGraph.GenerateRandom(8, 18, 0, 2)
 network.GenerateRandomSrcDst(3)
 
 #full network
@@ -21,7 +21,7 @@ c_dict = network.GetCapacityParam()
 
 #create two model generators for two formulation
 nmg = mg.RsModelGenerator(mg.LinearObjectiveGenerator(), mg.RouteConstraintsGenerator(), mg.LinearCapacityConstraintsGenerator())
-nmg_alt = mg.RsModelGenerator(mg.LinearObjectiveGenerator(), mg.ReformulatedConstraintsGenerator())
+nmg_alt = mg.RsModelGenerator(mg.QuadObjectiveGenerator(), mg.ReformulatedConstraintsGenerator())
 
 #create model instance from the generator
 rsm_base_instance = nmg.CreateInstance(f_list, sd_dict, n_list, a_list, c_dict, (0,3))
@@ -32,8 +32,8 @@ opt_glpk = sm.milpsolvers.GlpkSolver()
 opt_cplex = sm.miqppsolver.CplexSolver()
 opt_couenne = sm.minlpsolvers.CouenneSolver()
 
-objective, strains, routes, solver_output = opt_glpk.Solve(rsm_base_instance)
-objective_alt, strains_alt, routes_alt, solver_output_alt = opt_glpk.Solve(rsm_base_instance_alt)
+objective, strains, routes, solver_output = opt_couenne.Solve(rsm_base_instance)
+objective_alt, strains_alt, routes_alt, solver_output_alt = opt_cplex.Solve(rsm_base_instance_alt)
 
 #validate constraints violations
 viol = mg.FindConstraintsViolation(rsm_base_instance)
@@ -49,13 +49,13 @@ path_list_sol =  [ [edge for edge, value in route.items() if value == 1] for rou
 flow_list_sol = strains
 network.SetPath(*path_list_sol)
 network.SetFlows(*flow_list_sol)
-net.PlotNetwork(network)
+net.PlotNetwork(network, 'Original formulation')
 
 #put solution of the problem's alternative formulation into the network graph
 path_list_sol_alt =  [ [edge for edge, value in route.items() if value == 1] for route in routes_alt ]
 flow_list_sol_alt = strains_alt
 network.SetPath(*path_list_sol_alt)
 network.SetFlows(*flow_list_sol_alt)
-net.PlotNetwork(network)
+net.PlotNetwork(network, 'Alternative formulation')
 
-debug_val = 1
+net.drawer.ShowAll()
