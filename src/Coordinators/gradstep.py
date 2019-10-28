80265c7b994e5d65fe918b56278f3de477166827
Ignacy Ruksza <ignacy.ruksza@gmail.com>
2019-10-28 23:10:48 +0000

    Fixed bug in gradient level step rule.

--- src/Coordinators/gradstep.py
+++ src/Coordinators/gradstep.py
@@ -3,9 +3,7 @@ import pyomo.environ as pyo
 
 class IStepRule:
     def __init__(self, *args):
-        self.step = 0
-    def GetStep(self):
-        return self.step
+        pass
     def Reset(self):
         pass
     def UpdateData(self, **kwargs):
@@ -22,6 +20,8 @@ class IStepRule:
                 z_val = max(0, z_val)
             ret_val.append(z_val)
         return ret_val
+    def GetStep(self):
+        return 0
 
 class ConstantStepRule(IStepRule):
     def __init__(self, step):
@@ -34,42 +34,44 @@ class SquareSummableStepRule(IStepRule):
         self.nom = nom
         self.den = den
         self.n_iter = 0
+    def Reset(self):
+        self.n_iter = 0
     def GetStep(self):
         self.n_iter += 1
         self.step = self.nom / (self.den + self.n_iter)
         return self.step
-    def Reset(self):
-        self.n_iter = 0
 
 class DiminishingStepRule(IStepRule):
     def __init__(self, nom):
         self.nom = nom
         self.n_iter = 0
+    def Reset(self):
+        self.n_iter = 0
     def GetStep(self):
         self.n_iter += 1
         self.step = self.nom / math.sqrt(self.n_iter)
         return self.step
-    def Reset(self):
-        self.n_iter = 0
 
 class OptimalObjectiveStepRule(IStepRule):
-    def __init__(self, scale = 1.0):
+    def __init__(self, scale):
         self.n_iter = 0
-        self.scale = scale
         self.gradient_norm = 0
+        self.scale = scale
     def UpdateData(self, **kwargs):
         super().UpdateData(**kwargs)
         self.gradient_norm = sum([ gr**2 for gr in self.gradient])
-    def GetStep(self, dual_ub):
-        self.n_iter += 1
-        self.step = self.scale * (dual_ub - self.dual_val) / self.gradient_norm
-        return self.step
     def Reset(self):
         self.n_iter = 0
+        self.gradient_norm = 0
+    def SetUb(self, dual_ub):
+        self.dual_ub = dual_ub
+    def GetStep(self):
+        self.n_iter += 1
+        self.step = self.scale * (self.dual_ub - self.dual_val) / self.gradient_norm
+        return self.step
 
 class ObjectiveLevelStepRule(OptimalObjectiveStepRule):
     def __init__(self, R, scale = 1.0):
-        self.param_t = scale
         self.f_lev = {}
         self.f_rec = { 0: float('-inf') }
         self.x_rec = {}
@@ -80,16 +82,13 @@ class ObjectiveLevelStepRule(OptimalObjectiveStepRule):
         self.k = 0
         self.K = {1 : 1}
         self.l = 1
-        super().__init__(self.param_t)
+        super().__init__(scale)
 
-    def UpdateData(self, **kwargs):
-        super().UpdateData(**kwargs)
-
-    def InitializeApprox(self):
+    def InitializeLevelStep(self):
         self.delta[1] = math.sqrt(self.gradient_norm) * self.R
         self.k += 1
     
-    def AproximateUpperBound(self):
+    def IterateLevelStep(self):
         #step 1 : objective evaluation
         if self.dual_val > self.f_rec[self.k - 1]:
             self.f_rec[self.k] = self.dual_val
@@ -113,7 +112,7 @@ class ObjectiveLevelStepRule(OptimalObjectiveStepRule):
         else:
             #step 4 : oscilation detection
             if self.sigma[self.k] > self.R:
-                self.K[self.l + 1] = self.n_iter
+                self.K[self.l + 1] = self.k
                 self.sigma[self.k] = 0
                 self.delta[self.l + 1] = 0.5 * self.delta[self.l]
                 self.variables = self.x_rec[self.k]
@@ -122,26 +121,22 @@ class ObjectiveLevelStepRule(OptimalObjectiveStepRule):
         
         #step 5 : projections
         self.f_lev[self.k] = self.f_rec[self.K[self.l]] + self.delta[self.l]
-        ret_val = self.f_lev[self.k]
-        z = []
+        self.z = []
+        self.step = self.scale * (self.f_lev[self.k] - self.dual_val) / self.gradient_norm
+
         for indx, data in enumerate(self.variables):
             v,s = data
-            z_val = v + self.param_t * (self.f_lev[self.k] - self.dual_val) * self.gradient[indx] / self.gradient_norm
+            z_val = v +  self.step * self.gradient[indx]
             if s == '<=':
                 z_val = max(0, z_val)
-            z.append(z_val)
-        self.sigma[self.k + 1] = self.sigma[self.k] + math.sqrt( sum( [ (z - self.variables[indx][0])**2 for indx, z in enumerate(z) ] ) )
+            self.z.append(z_val)        
 
         #step 6 : path update
-        self.sigma[self.k + 1] += 0
+        self.sigma[self.k + 1] = self.sigma[self.k] + math.sqrt( sum( [ (z - self.variables[indx][0])**2 for indx, z in enumerate(self.z) ] ) )
         self.k += 1
 
-        return ret_val
-
-    def GetStep(self):
+    def GetVariables(self):
         if self.k == 0:
-            self.InitializeApprox()
-
-        ub = self.AproximateUpperBound()
-
-        return super().GetStep(ub)
+            self.InitializeLevelStep()
+        self.IterateLevelStep()
+        return self.z
